502183428521059520f153908bb3463c244c2218
gracebeste <grace.beste@gmail.com>
2024-12-29 00:32:10 -0500

    Update googletrends.py

--- data/googletrends.py
+++ data/googletrends.py
@@ -9,5 +9,5 @@ pytrends.build_payload(keywords, cat=0, timeframe='2013-01-01 2024-12-28', geo='
 
 # Fetch interest over time
 data = pytrends.interest_over_time()
-data.to_csv('google_trends_sunscreen.csv')
-print("Google Trends data saved.")
+data.to_csv('/Users/gracebeste/documents/trends-sunscreen-analysis/google_trends_sunscreen.csv')
+print("Google Trends sunscreen keyword trend data saved.")
