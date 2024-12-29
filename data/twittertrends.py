06f476539251c0d0ec4e5d8ef59e1e93a02c0847
gracebeste <grace.beste@gmail.com>
2024-12-29 00:37:36 -0500

    Create twittertrends.py

---
+++ data/twittertrends.py
@@ -0,0 +1,23 @@
+import tweepy
+import json
+
+# Twitter API keys
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+
+# Authenticate
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Search tweets
+query = "#sunscreen OR #skincare"
+tweets = tweepy.Cursor(api.search_tweets, q=query, lang="en", since="2013-01-01").items(1000)
+
+# Save tweets to JSON
+tweets_data = [{"text": tweet.text, "created_at": tweet.created_at} for tweet in tweets]
+with open('tweets_sunscreen.json', 'w') as file:
+    json.dump(tweets_data, file)
+print("Twitter data saved.")
