ffd11f63a56c3406af291b1d195ae16eb87b8103
gracebeste <grace.beste@gmail.com>
2024-12-30 21:24:51 -0500

    Update noaatrends.py

--- data/noaatrends.py
+++ data/noaatrends.py
@@ -1,14 +1,20 @@
 import requests
+import json
+import logging
+import http.client
+
+http.client.HTTPConnection.debuglevel = 1
+logging.basicConfig(level=logging.DEBUG)
 
 # NOAA API details
 base_url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data"
-headers = {"token": "YOUR_NOAA_API_TOKEN"}
+headers = {"token": "kbMPIvfqWqDZiIPlrdVAxhDJMufeubQG"}
 
 # Parameters
 params = {
     "datasetid": "GHCND",  # Global Historical Climatology Network Daily
     "locationid": "FIPS:US",  # US
-    "startdate": "2013-01-01", # Timeframe start
+    "startdate": "2023-12-31", # Timeframe start. Date range cannot be >1 year.
     "enddate": "2024-12-30", # Timeframe end
     "datatypeid": "TMAX",  # Max temperature as an example
     "units": "metric", 
@@ -24,3 +30,4 @@ if response.status_code == 200:
     print("NOAA data saved.")
 else:
     print(f"Error: {response.status_code}")
+    print("Response:", response.text) 
