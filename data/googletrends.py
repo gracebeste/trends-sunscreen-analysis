0c55370e9be459ac94a026cdf7c36bd039b7c69f
gracebeste <grace.beste@gmail.com>
2024-12-31 17:52:12 -0500

    Update googletrends.py

--- data/googletrends.py
+++ data/googletrends.py
@@ -5,7 +5,7 @@ pytrends = TrendReq(hl='en-US', tz=360)
 
 # Define keywords and timeframe
 keywords = ["sunscreen", "SPF", "UV protection", "skincare"]
-pytrends.build_payload(keywords, cat=0, timeframe='2013-01-01 2024-12-28', geo='US', gprop='')
+pytrends.build_payload(keywords, cat=0, timeframe='2020-01-01 2024-12-31', geo='US', gprop='')
 
 # Fetch interest over time
 data = pytrends.interest_over_time()
